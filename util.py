c0a07b49bdc73d8c835a297df02cdf10b09e4392
suryanarayanan21 <suryanarayanan.kviisc@gmail.com>
2026-03-12 19:09:34 +0000

    Softmax

--- util.py
+++ util.py
@@ -1,7 +1,18 @@
 import pandas as pd
+from scipy.special import softmax
 
 distribution = pd.read_csv("./distribution2.csv", index_col=0)
+tokens = distribution["Token"]
+
 distribution = distribution.drop(columns=["Token", "Cluster"])
+
+classes = distribution.columns
+
+probs = softmax(distribution.to_numpy(), axis = 1)
+probs_df = pd.DataFrame(probs, index=tokens, columns=classes)
+probs_df['Cluster'] = probs_df.idxmax(axis=1)
+probs_df.to_csv("./distribution_softmax.csv")
+
 distribution['Cluster'] = distribution.idxmax(axis = 1)
 distribution['Token'] = distribution.index
 
